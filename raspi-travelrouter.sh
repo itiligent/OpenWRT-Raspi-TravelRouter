678d6362a73f40d05165f51c32a8c5e422985e9d
Itiligent <94789708+itiligent@users.noreply.github.com>
2024-12-28 15:59:48 +1100

    support python 3.11 and 3.12 build dependencies

--- raspi-travelrouter.sh
+++ raspi-travelrouter.sh
@@ -8,35 +8,47 @@
 clear
 
 # Prepare text output colours
-LYELLOW='\033[0;93m'
+CYAN='\033[0;36m'
 LRED='\033[0;91m'
-NC='\033[0m' #No Colour
+LYELLOW='\033[0;93m'
+NC='\033[0m' # No Colour
 
-if ! [[ $(id -u) = 0 ]]; then
+# Make sure the user is NOT running this script as root
+if [[ $EUID -eq 0 ]]; then
     echo
-    echo -e "${LRED}Please run this script as sudo.${NC}" 1>&2
-    echo
+    echo -e "${LRED}This script must NOT be run as root, it will prompt for sudo when needed." 1>&2
+    echo -e ${NC}
     exit 1
 fi
 
-echo -e "${LYELLOW}Checking for curl...${NC}"
-apt-get update -qq
-apt-get install curl -qq -y
+# Check if sudo is installed. (Debian does not always include sudo by default.)
+if ! command -v sudo &> /dev/null; then
+    echo "${LRED}Sudo is not installed. Please install sudo."
+    echo -e ${NC}
+    exit 1
+fi
 
+# Make sure the user running setup is a member of the sudo group
+if ! id -nG "$USER" | grep -qw "sudo"; then
+    echo
+    echo -e "${LRED}The current user (${USER}) must be a member of the 'sudo' group. Run: sudo usermod -aG sudo ${USER}${NC}" 1>&2
+    exit 1
+fi
+
+# Trigger a prompt for sudo so it is used only where needed
+echo
+echo -e "${CYAN}Script requires sudo privileges for some actions${NC}"
+echo
+sudo sudo -v
+echo
+echo -e "${CYAN}Checking for curl...${NC}"
+sudo apt-get update -qq && sudo apt-get install curl -qq -y
 clear
 
 #######################################################################################################################
-# User input variables
+# ADD YOUR CUSTOM PACKAGE RECIPE HERE
 #######################################################################################################################
 
-# Mandatory static user input to determine versions
-    VERSION=""               # "" = snapshot or enter specifc version
-    TARGET="bcm27xx"         # Set the CPU build target, bcm27xx is the value for Raspi 3 & 4
-    ARCH="bcm2711"           # Set to ARCH=bcm2710 for Raspi3 or ARCH=bcm2711 for Rapi4 
-    IMAGE_PROFILE="rpi-4"    # Set to rpi-3 or rpi-4 | For available profiles run $SOURCE_DIR/make info
-    RELEASE_URL="https://downloads.openwrt.org/releases/" # Where to obtain latest stable version number
-    BUILD_LOG="$(pwd)/build.log"
-    
 # Package Selections. Provide your specific recipe of custom OWRT packages for the custom build here.
     CUSTOM_PACKAGES="-wpad-basic-mbedtls wpad-mbedtls -dnsmasq dnsmasq-full \
     auc curl luci luci-app-attendedsysupgrade luci-app-mwan3 luci-app-sqm luci-app-travelmate mwan3 nano qrencode sqm-scripts sqm-scripts-extra tcpdump travelmate \
@@ -51,30 +63,38 @@ clear
     # 5th line above contains RTL8812 usb driver test packages. Remove & add your specific usb wifi chipset packages here
 
 #######################################################################################################################
-# Script user prompts
+# User input variables
 #######################################################################################################################
 
-echo -e ${LYELLOW}
-echo "Image Builder activity will be logged to ${BUILD_LOG}"
-echo
+# Mandatory static user input to determine versions
+    VERSION=""               # Blank "" triggers user prompt for a specifc OWRT version or snapshot.
+    TARGET="bcm27xx"         # Set the CPU build target, bcm27xx is the value for Raspi 3 & 4
+    ARCH="bcm2711"           # Set to ARCH=bcm2710 for Raspi3 or ARCH=bcm2711 for Rapi4 
+    IMAGE_PROFILE="rpi-4"    # Set to rpi-3 or rpi-4 | For available profiles run $SOURCE_DIR/make info
+    RELEASE_URL="https://downloads.openwrt.org/releases/" # Where to obtain latest stable version number
+    BUILD_LOG="$(pwd)/build.log"
+    IMAGE_TAG=""             # ID tag is added to the completed image filename to uniquely identify the built image(s)
+  
+#######################################################################################################################
+# Script prompt variables - do not edit unless expert
+#######################################################################################################################
 
 # Prompt for the desired OWRT version
 if [[ -z ${VERSION} ]]; then
-LATEST_RELEASE=$(curl -s "$RELEASE_URL" | grep -oP "([0-9]+\.[0-9]+\.[0-9]+)" | sort -V | tail -n1)
-echo
-    echo -e "${LYELLOW}Enter OpenWRT version to build:${NC}"
+    LATEST_RELEASE=$(curl -s "$RELEASE_URL" | grep -oP "([0-9]+\.[0-9]+\.[0-9]+)" | sort -V | tail -n1)
+    echo
+    echo -e "${CYAN}Enter OpenWRT version to build:${NC}"
     while true; do
-        read -p "    Enter a version number (latest stable release is $LATEST_RELEASE), or leave blank for latest snapshot: " VERSION
+        read -p "    Enter a release version number (latest stable release = $LATEST_RELEASE), or hit enter for latest snapshot: " VERSION
         [[ "${VERSION}" = "" ]] || [[ "${VERSION}" != "" ]] && break
     done
     echo
 fi
 
 # Create a custom image name tag
-IMAGE_TAG=""
 if [[ -z ${IMAGE_TAG} ]]; then
-echo
-    echo -e "${LYELLOW}Custom image filename identifier:${NC}"
+    echo
+    echo -e "${CYAN}Custom image filename identifier:${NC}"
     while true; do
         read -p "    Enter text to include in the image filename [Enter for \"custom\"]: " IMAGE_TAG
         [[ "${IMAGE_TAG}" = "" ]] || [[ "${IMAGE_TAG}" != "" ]] && break
@@ -89,59 +109,87 @@ fi
 # Setup the image builder working environment
 #######################################################################################################################
 
-# Select the OWRT version to build.
+# Dynamically create the OpenWRT download link
 if [[ ${VERSION} != "" ]]; then
     BUILDER="https://downloads.openwrt.org/releases/${VERSION}/targets/${TARGET}/${ARCH}/openwrt-imagebuilder-${VERSION}-${TARGET}-${ARCH}.Linux-x86_64.tar.xz"
 else
-    BUILDER="https://downloads.openwrt.org/snapshots/targets/${TARGET}/${ARCH}/openwrt-imagebuilder-${TARGET}-${ARCH}.Linux-x86_64.tar.xz" # Current snapshot
+    BUILDER="https://downloads.openwrt.org/snapshots/targets/${TARGET}/${ARCH}/openwrt-imagebuilder-${TARGET}-${ARCH}.Linux-x86_64.tar.zst" # Current snapshot
 fi
 
 # Configure the build paths
-    SOURCE_FILE="${BUILDER##*/}" # Separate the tar.xz file name from the source download link
-    SOURCE_DIR="${SOURCE_FILE%%.tar.xz}" # Get the uncompressed tar.xz directory name and set as the source dir
-    BUILD_ROOT="$(pwd)/openwrt_build_output"
-    OUTPUT="${BUILD_ROOT}/firmware_images"
-    INJECT_FILES="$(pwd)/openwrt_inject_files"
+SOURCE_FILE="${BUILDER##*/}" # Separate the tar.xz file name from the source download link
+BUILD_ROOT="$(pwd)/openwrt_build_output"
+OUTPUT="${BUILD_ROOT}/firmware_images"
+VMDIR="${BUILD_ROOT}/vm"
+INJECT_FILES="$(pwd)/openwrt_inject_files"
+BUILD_LOG="${BUILD_ROOT}/owrt-build.log" # Creates a build log in the local working directory
+
+# Set SOURCE_DIR based on download file extension (annoyingly snapshots changed to tar.zst. vs releases are tar.xz)
+SOURCE_EXT="${SOURCE_FILE##*.}"
+if [[ "${SOURCE_EXT}" == "xz" ]]; then
+    SOURCE_DIR="${SOURCE_FILE%.tar.xz}"
+	EXTRACT="tar -xJvf"
+elif [[ "${SOURCE_EXT}" == "zst" ]]; then
+    SOURCE_DIR="${SOURCE_FILE%.tar.zst}"
+	EXTRACT="tar -I zstd -xf"
+else
+    echo "Unsupported file extension: ${SOURCE_EXT}"
+fi
 
 #######################################################################################################################
 # Begin script build actions
 #######################################################################################################################
+
 # Clear out any previous builds
-    rm -rf "${BUILD_ROOT}"
-    rm -rf "${SOURCE_DIR}"
+rm -rf "${BUILD_ROOT}"
+rm -rf "${SOURCE_DIR}"
 
 # Create the destination directories
-    mkdir -p "${BUILD_ROOT}"
-    mkdir -p "${OUTPUT}"
-    mkdir -p "${INJECT_FILES}"
-    chown -R $SUDO_USER $INJECT_FILES
-    chown -R $SUDO_USER $BUILD_ROOT
+mkdir -p "${BUILD_ROOT}"
+mkdir -p "${OUTPUT}"
+mkdir -p "${INJECT_FILES}"
+if [[ ${CREATE_VM} = true ]] && [[ ${IMAGE_PROFILE} = "generic" ]]; then mkdir -p "${VMDIR}" ; fi
 
 # Option to pre-configure images with injected config files
-    echo -e ${LYELLOW}
-    read -p $"Copy optional config files to ${INJECT_FILES} now for inclusion into the new image. Enter to begin build..."
-    echo -e ${NC}
+echo -e "${LYELLOW}"
+echo -e "    [Optional] TO BAKE A CUSTOM CONFIG INTO YOUR OWRT IMAGE"
+echo -e "    copy your OWRT backup config files to ${CYAN}${INJECT_FILES}${LYELLOW} before hitting enter..."
+echo
+read -p "    Press ENTER to begin the OWRT build..."
+echo -e "${NC}"
 
 # Install OWRT build system dependencies for recent Ubuntu/Debian.
 # See here for other distro dependencies: https://openwrt.org/docs/guide-developer/toolchain/install-buildsystem
-    sudo apt-get update  2>&1 | tee -a ${BUILD_LOG}
+
+# Get the Python 3 version
+PYTHON_VERSION=$(python3 --version 2>&1 | awk '{print $2}')
+
+# Split the Python3 version into major, minor, and patch components
+IFS='.' read -r -a VERSION_PARTS <<< "$PYTHON_VERSION"
+MAJOR=${VERSION_PARTS[0]}
+MINOR=${VERSION_PARTS[1]}
+
+# Compare the distro Python3 version and install the correct build dependencies
+if (( MAJOR < 3 )) || (( MAJOR == 3 && MINOR <= 11 )); then
+    echo "Python version is less than or equal to 3.11"
     sudo apt-get install -y build-essential clang flex bison g++ gawk gcc-multilib g++-multilib \
-    gettext git libncurses-dev libssl-dev python3-distutils rsync unzip zlib1g-dev file wget qemu-utils 2>&1 | tee -a ${BUILD_LOG}
+    gettext git libncurses5-dev libssl-dev python3-distutils python3-setuptools rsync unzip zlib1g-dev file wget qemu-utils zstd  2>&1 | tee -a ${BUILD_LOG}
+else
+    echo "Python version is 3.12 or above"
+	sudo apt-get install -y build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
+    python3-setuptools rsync swig unzip zlib1g-dev file wget 2>&1 | tee -a ${BUILD_LOG}
+fi
 
 # Download the image builder source if we haven't already
-if [ ! -f "${BUILDER##*/}" ]; then
+if [ ! -f "${SOURCE_FILE}" ]; then
     wget -q --show-progress "$BUILDER"
-    tar xJvf "${BUILDER##*/}" --checkpoint=.100 2>&1 | tee -a ${BUILD_LOG}
+    ${EXTRACT} "${SOURCE_FILE}" | tee -a ${BUILD_LOG}
 fi
 
-# Uncompress if the source tar.xz exists but there is no uncompressed source directory (was cleared for a fresh build).
-if [ -n "${SOURCE_DIR}" ]; then
-    tar xJvf "${BUILDER##*/}" --checkpoint=.100 2>&1 | tee -a ${BUILD_LOG}
+# Uncompress if the source tarball exists but there is no uncompressed source directory (saves re-download when build directories are cleared for a fresh build).
+if [ -f "${SOURCE_FILE}" ]; then
+     ${EXTRACT} "${SOURCE_FILE}" | tee -a ${BUILD_LOG}
 fi
-
-# Remove sudo access limits on source download
-    chown -R $SUDO_USER:root $SOURCE_FILE
-    chown -R $SUDO_USER:root $SOURCE_DIR
     
 # Start a clean image build with the selected packages
     cd $(pwd)/"${SOURCE_DIR}"/
